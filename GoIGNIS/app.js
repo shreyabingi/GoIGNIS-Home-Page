3d702f33be90156f6eb1479d854615d42e0ac5db
shreyabingi <163561869+shreyabingi@users.noreply.github.com>
2024-09-30 12:29:25 +0530

    Uploaded my project

---
+++ GoIGNIS/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $('.banner-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        speed: 500
+    });
+});
+
+
+$(document).ready(function() {
+    $('.service-img-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        fade: true,
+        asNavFor: '.service-description-sec',
+        autoplay: true, 
+        autoplaySpeed: 2000 
+    });
+
+    $('.service-description-sec').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        fade: true,
+        asNavFor: '.service-img-slider',
+        autoplay: true, 
+        autoplaySpeed: 2000 
+    });
+
+    // Sync active class
+    $('.service-img-slider').on('afterChange', function(event, slick, currentSlide) {
+        $('.service-list div').removeClass('active');
+        $('.service-list div[data-index="' + currentSlide + '"]').addClass('active');
+    });
+
+    // Click service list items
+    $('.service-list div').click(function() {
+        var index = $(this).data('index');
+        $('.service-img-slider').slick('slickGoTo', index);
+        $('.service-description-sec').slick('slickGoTo', index);
+    });
+});
+
